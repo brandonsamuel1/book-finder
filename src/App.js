bba179080016de91e94df8903b6fb659122f36fd
brandon-earls <brandonsamuel72@gmail.com>
2022-03-15 12:38:28 -0700

    google books api working, getting data back from api into console

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [searchText, setSearchText] = useState('')
+  const [bookList, setBookList] = useState([])
+
+  const searchBook = (title) => {
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${title}&key=${process.env.REACT_APP_API_KEY}`)
+      .then(response => response.json())
+      .then(result => {
+        console.log(result.items)
+      })
+  }
+
+  useEffect(() => {
+    searchBook('snoppy')
+  }, [])
+
+
   return (
     <div className="App">
       Hello App
